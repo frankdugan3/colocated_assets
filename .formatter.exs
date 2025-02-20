11a471ca1bf52d4cc2a76da04592ae70eed44b5d
Frank Dugan III <frank3@duganator.com>
2025-02-19 20:40:01 -0600

    chore: dogfood formatter

--- .formatter.exs
+++ .formatter.exs
@@ -3,5 +3,6 @@ locals = [assign_colocated_eex: 2]
 [
   locals_without_parens: locals,
   exports: [locals_without_parens: locals],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [ColocatedAssets.Formatter]
 ]
